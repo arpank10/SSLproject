615c62e3cdbcaa2980e26ec94744b675cc47c22b
arpank10 <arpan97official@gmail.com>
2017-11-19 21:50:42 +0530

    changed urls so that forms appear always

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -8,13 +8,13 @@ app_name = 'mywebsite'
 
 urlpatterns = [
     url(r'^$', core_views.home, name='home'),
-    url(r'^details/(?P<person>[a-zA-Z0-9_]+)$', core_views.showdetail , name='detail'),
-    url(r'^details/update/(?P<whoami>[a-zA-Z0-9_]+)$', core_views.updatedetails , name='update'),
+    url(r'^(?P<person>[a-zA-Z0-9_]+)$', core_views.showdetail , name='detail'),
+    url(r'^update/(?P<whoami>[a-zA-Z0-9_]+)$', core_views.updatedetails , name='update'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'mywebsite:login'}, name='logout'),
     url(r'^signup/$', core_views.signup, name='signup'),
-    url(r'^details/(?P<person>[a-zA-Z0-9_]+)/courses$', core_views.courses , name='courses') ,
-    url(r'^details/(?P<course>[a-zA-Z0-9_]+)/courses/upload$', core_views.courses_upload , name='courses_upload'),
+    url(r'^(?P<person>[a-zA-Z0-9_]+)/courses$', core_views.courses , name='courses') ,
+    url(r'^(?P<course>[a-zA-Z0-9_]+)/courses/upload$', core_views.courses_upload , name='courses_upload'),
     url(r'^download/(?P<file_name>.+)$', core_views.download_file , name='download')
 
 ]
